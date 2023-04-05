67bce4f9e06b434a9a6c1dfaabed5ee4579d61e3
Cole Perry <colecperry@gmail.com>
2023-04-05 09:14:18 -0700

    Done

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -1,3 +1,5 @@
+import ipdb
+
 spicy_foods = [
     {
         "name": "Green Curry",
@@ -17,22 +19,54 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
+    each_food = [food.get("name") for food in spicy_foods]
+    return each_food
+    # each_food = []
+    # for food in spicy_foods:
+        # each_food.append(food.get("name"))
+    # return each_food
     pass
 
 def get_spiciest_foods(spicy_foods):
-    pass
+   spiciest_foods = [food for food in spicy_foods if food.get("heat_level") > 5]
+   return spiciest_foods
+
+    # spiciest_foods = []
+    # for food in spicy_foods:
+    #     if food.get('heat_level', 0) > 5:
+    #         spiciest_foods.append(food)
+    #     return spiciest_foods
+    # pass
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+            food_name = food.get("name")
+            food_cuisine = food.get("cuisine")
+            food_heat = "🌶" * food.get("heat_level")
+            print(f'{food_name} ({food_cuisine}) | Heat Level: {food_heat}')
+            pass
+    
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food.get("cuisine") == cuisine:
+            return food
 
 def print_spiciest_foods(spicy_foods):
-    pass
+        for food in spicy_foods:
+            if food.get("heat_level") > 5:
+                food_name = food.get("name")
+                food_cuisine = food.get("cuisine")
+                food_heat = "🌶" * food.get("heat_level")
+                print(f'{food_name} ({food_cuisine}) | Heat Level: {food_heat}')
+            pass
 
 def get_average_heat_level(spicy_foods):
+    heat_list = [food.get("heat_level") for food in spicy_foods]
+    heat_average = sum(heat_list)/len(heat_list)
+    return heat_average
     pass
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    return spicy_foods + [spicy_food]
